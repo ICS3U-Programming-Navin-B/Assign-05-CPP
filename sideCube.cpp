c8e799d8e5f80730ba18d21359b0cde47d43e005
Ubuntu <ubuntu@ip-172-31-7-242.ec2.internal>
2022-01-25 22:51:02 +0000

    added sideCube.cpp

---
+++ sideCube.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+    int main()
+    {
+    	int sid1;
+    	float volcu;
+        cout<<" Put the number for the side of your cube ! : ";
+    	cin>>sid1;
+    	volcu=(sid1*sid1*sid1);
+        cout<<" The volume of a cube is : "<< volcu << endl;
+        cout << endl;
+        return 0;
+    }
